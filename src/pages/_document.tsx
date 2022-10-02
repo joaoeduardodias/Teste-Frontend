1c71c3bc2d44bf018b8de118d2f9bae973e6f968
joaoeduardodias <joaoeduardodias123@gmail.com>
2022-10-02 14:43:18 -0300

    feat: initial Config

---
+++ src/pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+
+          <meta
+            name="description"
+            content="PontoGo, sistema para controle de ponto de seus colaboradores. Com o PontoGo seus
+            colaboradores poderão bater seus pontos de forma fácil e rápida, possuindo também uma Dashboard intuitiva."
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => <h1>Hello word</h1>
+
+export default Home
