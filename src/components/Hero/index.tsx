2cec8258655a49a5c3810a831e90be389376484f
joaoeduardodias <joaoeduardodias123@gmail.com>
2022-10-04 11:03:23 -0300

    style: add responsive of component hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,16 +1,36 @@
-import { Button, Flex, Heading, HStack, Image, Text } from '@chakra-ui/react'
+import {
+  Button,
+  Flex,
+  Heading,
+  HStack,
+  Image,
+  Text,
+  // eslint-disable-next-line prettier/prettier
+  useBreakpointValue,
+} from '@chakra-ui/react'
 
 export function Hero(): JSX.Element {
+  const showImageIllustration = useBreakpointValue({ base: false, lg: true })
+
   return (
     <Flex
       as="section"
       mt="148px"
       w="90%"
+      h={['auto', 'auto', 'auto', '23.68rem', '31.68rem']}
       maxW="1140px"
       mx="auto"
-      position="relative"
+      overflow="hidden"
+      gap={['0', '0', '0', '0', '3rem']}
     >
-      <Flex direction="column" maxW="520px">
+      <Flex
+        direction="column"
+        maxW={['100%', '100%', '100%', '520px', '520px']}
+        h="100%"
+        justify="center"
+        align={['center', 'center', 'center', 'initial', 'initial']}
+        textAlign={['center', 'center', 'center', 'initial', 'initial']}
+      >
         <Text
           as="p"
           textTransform="uppercase"
@@ -46,13 +66,16 @@ export function Hero(): JSX.Element {
           </Button>
         </HStack>
       </Flex>
-      <Flex position="absolute" right="0" top="-20">
+
+      {showImageIllustration && (
         <Image
+          w="50%"
+          objectFit="cover"
           src="./illustration.svg"
           alt="Homem com óculos de realidade virtual perto de um ícone de foguete,
-         um notebook e um calendário"
+        um notebook e um calendário"
         />
-      </Flex>
+      )}
     </Flex>
   )
 }
